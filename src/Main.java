f6490fa30242c06f6de8e0bd22645e892868b37c
akdiscord1234 <117234499+akdiscord1234@users.noreply.github.com>
2026-05-20 11:53:23 -0400

    Save 5.20
    Created initial window and panels

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.awt.*;
+import javax.swing.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("FRAME TITLE");
+        JPanel outputScreen = new JPanel();
+        JPanel inputPanel = new JPanel();
+
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+
+        frame.setLayout(new GridLayout(2, 1));
+
+        outputScreen.setBackground(Color.WHITE);
+        frame.add(outputScreen);
+        inputPanel.setLayout(new GridLayout(4, 3));
+        frame.add(inputPanel);
+
+
+        //adding components
+
+        JButton oneBtn = new JButton("1");
+
+
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, screen.width/3, screen.height/2);
+        frame.setVisible(true);
     }
 }
